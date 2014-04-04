4cc39a8183ba6f12fddfcca24eecf2a3aa918713
ianlyons <ilyons@gmail.com>
2014-04-04 16:41:30 -0700

    Substrate service that returns patient demographics, vitals, and lab results using getPatientData promise chain.

--- app/js/app.js
+++ app/js/app.js
@@ -14,7 +14,10 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]).
-run(['$rootScope', function($rootScope) {
+run(['$rootScope','algorithmSvc','substrate', function($rootScope, algorithmSvc, substrate) {
   $rootScope.patientId = 3230000;
   $rootScope.calculator = 'JNC8';
+
+  // all calculator logic would live in algorithmSvc
+  // algorithmSvc(substrate.getPatientData($rootScope.patientId), 'JNC8');
 }]);
