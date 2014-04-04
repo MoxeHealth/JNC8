db62e00526fff5cb9f880b155196e79b616d6c96
ianlyons <ilyons@gmail.com>
2014-04-04 16:41:30 -0700

    Substrate service that returns patient demographics, vitals, and lab results using getPatientData promise chain.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,15 @@
 
 angular.module('myApp.controllers', [])
   .controller('dataEntryCtrl', ['$rootScope', 'substrate',  function($rootScope, substrate) {
-    substrate.getPatientDemographics($rootScope.patientId);
-    substrate.getLabs($rootScope.patientId);
-    substrate.getVitals($rootScope.patientId);
+    console.log('Controller loaded.');
+    substrate.getPatientData($rootScope.patientId, function(patientData) {
+      console.log('Into getPD callback...')
+      console.log(patientData);
+    });
+    // $rootScope.patientData = substrate.getPatientData($rootScope.patientId);
+    // console.log($rootScope.patientData);
+    // substrate.getLabs($rootScope.patientId);
+    // substrate.getVitals($rootScope.patientId);
   }])
 
   .controller('MyCtrl2', [function() {
--- app/js/services.js
+++ app/js/services.js
@@ -4,69 +4,76 @@
 
 angular.module('myApp.services', [])
   .value('version', '0.1')
-  .factory('substrate', ['$http', function($http) {
+  .factory('substrate', ['$http', '$q', function($http, $q) {
     var apiPaths = {
       demographics: '/patient/demographics',
       vitals: '/encounter/vitals',
       labs: '/patient/labs'
     };
 
-    return {
-      getPatientDemographics: function(patientId) {
-        console.log('getPatientDemographics is being run.');
-        $http({
-          url: apiPaths.demographics,
-          method: 'POST',
-          data: {
-            'Value': patientId,
-            'Type': 'MRN'
-          }
-        }).success(function(data, status, headers, config) {
-          console.log('GetPatientDemographics success.');
-          console.dir(data);
-        }).error(function(data, status, headers, config) {
-          console.log('GetPatientDemographics error.');
-        });
-      },
 
-      getLabs: function(patientId) {
-        console.log('getLabs is being run.');
-        $http({
-          url: apiPaths.labs,
-          method: 'POST',
-          data: {
-            'Value': patientId,
-            'Type': 'MRN'
-          }
-        }).success(function(data, status, headers, config) {
-          console.log('GetLabs success.');
-          console.dir(data);
-        }).error(function(data, status, headers, config) {
-          console.log('GetLabs error.');
-        });
-      },
+    var getPatientData = function(patientId, callback){
+      console.log('into getPatientData');
 
-      getVitals: function(patientId) {
-        console.log('getVitals is being run.');
-        $http({
-          url: apiPaths.vitals,
-          method: 'POST',
-          data: {
-            'Value': patientId,
-            'Type': 'MRN'
-          }
-        }).success(function(data, status, headers, config) {
-          console.log('GetVitals success.');
-          console.dir(data);
-        }).error(function(data, status, headers, config) {
-          console.log('GetVitals error.');
-        });
-      }
+      var result = $q.all({
+        demographics: getPatientDemographics(patientId),
+        vitals: getVitals(patientId),
+        lab: getLabs(patientId)
+      });
+
+      return result.then(function(response) {
+        var patientData = {
+          demographics: response.demographics.data,
+          vitals: response.vitals.data,
+          lab: response.lab.data
+        };
+        return callback(patientData);
+      });
     };
-  }])
-  .factory('algorithmSvc', ['substrate', function(substrate) {
-    substrate.getPatientData($rootScope.patientId).success(function() {
 
-    });
+    var getPatientDemographics = function(patientId) {
+      console.log('getPatientDemographics is being run.');
+      return $http({
+        url: apiPaths.demographics,
+        method: 'POST',
+        data: {
+          'Value': patientId,
+          'Type': 'MRN'
+        }
+      });
+    };
+
+    var getLabs = function(patientId) {
+      console.log('getLabs is being run.');
+      return $http({
+        url: apiPaths.labs,
+        method: 'POST',
+        data: {
+          'Value': patientId,
+          'Type': 'MRN'
+        }
+      });
+    };
+
+    var getVitals = function(patientId) {
+      console.log('getVitals is being run.');
+      return $http({
+        url: apiPaths.vitals,
+        method: 'POST',
+        data: {
+          'Value': patientId,
+          'Type': 'MRN'
+        }
+      });
+    };
+
+    return {
+      getPatientData: getPatientData,
+      getVitals: getVitals,
+      getLabs: getLabs
+    };
+
+  }])
+  .factory('algorithmSvc', [function() {
   }])
   ;
