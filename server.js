e576f1f5f5c2be68edaf5ad74d241a671507c44e
Hack Reactor Students <accounts@hackreactor.com>
2014-04-04 13:57:11 -0700

    Substrate service returns data from Moxe substrate API.

--- server.js
+++ server.js
@@ -3,46 +3,28 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
 
-var baseUrl = 'http://substratestaging.moxehealth.com/api/2013-1/get/patient';
-var apiPaths = {
-  demographics: 'demographics',
-  vitals: 'vitals',
-  labs: 'labs'
-};
+
+// API information and headers
+var baseUrl = 'http://substratestaging.moxehealth.com/api/2013-1/get';
 var unPassEncoded = 'SFJKU0M4VGVzdEFwcDowN2FhODJiZTI4ODY=';
 var headers = {
     'Content-type': 'application/json',
     'Authorization': 'Basic ' + unPassEncoded,
     'VendorID': 1, //Sandbox ID
     'OrganizationID': 3, //testing ID
-    'EHDUserId': 'terry', //testing user
+    'EHRUserId': 'terry', //testing user
     'ApplicationKey': 'b6956ad2-ca01-45e6-ab57-4b51b99e70df'
   };
 
-// var setHeaders = function(req, res, next){
-//   console.log("setHeaders being called");
 
-//   next();
-// };
-
-app.use(express.static(__dirname)); //maps to 'public'
+app.use(express.static(__dirname));
 app.use(bodyParser());
 
 app.post('/*',  function(req, res){
   console.log("Serving app.post...");
   var url = baseUrl + req.url;
-  res.writeHead(200, headers);
   console.log("The url: " + url);
-
-  request.post({uri: url, json: req.body, headers: headers}, function (e, r, body) {
-    if(e){
-      console.log('e', e);
-    }else{
-      // console.log('r', r);
-      req.pipe(r).pipe(res);
-      console.log('body', body);
-    }
-  });
+  req.pipe(request.post({uri: url, json: req.body, headers:headers})).pipe(res);
 });
 
 app.get('/', function(req,res){
