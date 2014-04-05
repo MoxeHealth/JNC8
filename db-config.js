04b9eb81058990df167f680f38e8a2c0c7335564
skeller88 <skeller88@gmail.com>
2014-04-05 16:37:34 -0700

    Insert encounter data into SQL database.

--- db-config.js
+++ db-config.js
@@ -2,17 +2,11 @@ var mysql = require('mysql');
 
 var host = process.env.DB_HOST || 'localhost';
 
-var connection = mysql.createConnection({
+exports.connection = mysql.createConnection({
   host: host,
   database: 'JNC8',
   user: 'root',
   password: ''
 });
 
-connection.connect();
-
-connection.query('SELECT * FROM `test`', function(err, rows, fields) {
-  if(err) throw err;
-
-  console.log(rows);
-};
+exports.connection.connect();
--- server.js
+++ server.js
@@ -21,11 +21,57 @@ var headers = {
 app.use(express.static(__dirname));
 app.use(bodyParser());
 
+app.get('/db/encounters',  function(req, res){
+  console.log('get db/encounters');
+  var ptId = req.body.ptId;
+
+  db.connection.query('SELECT * FROM `encounters` WHERE `patient_id` = ' + ptId, function(err, data){
+    if(err){
+      res.send(err);
+      console.log('get from db failed');
+    }else{
+      res.send(data);
+      console.log('data', data);
+    }
+  });
+});
+
+app.post('/db/encounters',  function(req, res){
+  console.log('post db/encounters');
+  var ptId = req.body.ptId;
+  var encounterDate = db.connection.escape(new Date().toISOString().slice(0, 19).replace('T', ' '));
+  var bloodPressure = db.connection.escape(JSON.stringify(req.body.encounter.bloodPressure));
+  var medicationsPrescribed = db.connection.escape(JSON.stringify(req.body.encounter.medicationsPrescribed));
+
+  console.log('ptId', ptId);
+  console.log('encounterDate', encounterDate);
+  console.log('bloodPressure', bloodPressure);
+  console.log('medicationsPrescribed', medicationsPrescribed);
+
+  db.connection.query('INSERT INTO `encounters` (patient_id, encounter_date, blood_pressure, medications_prescribed) VALUES (' + ptId + ',' + encounterDate + ',' + bloodPressure + ',' +medicationsPrescribed + ')', function(err, data){
+    if(err){
+      console.log('insert failed');
+      console.log(err);
+      res.send(err);
+    }else{
+      res.send(data)
+    }
+  });
+});
+
+//SELECT '3430000' FROM `patients` LEFT JOIN (`encounters`) ON (`encounters`.id_patients = `patients`.id)
+
 app.post('/*',  function(req, res){
   console.log("Serving app.post...");
   var url = baseUrl + req.url;
   console.log("The url: " + url);
   req.pipe(request.post({uri: url, json: req.body, headers:headers})).pipe(res);
+});
+
+app.get('/db',  function(req, res){
+  console.log("Serving app.get from db");
+  var url = baseUrl + req.url;
+  console.log("The url: " + url);
 });
 
 app.get('/', function(req,res){
