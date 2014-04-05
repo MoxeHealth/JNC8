828b012f65e6f3db9400cccd58c0e762503abccd
Hack Reactor Students <github@hackreactor.com>
2014-04-05 10:39:17 -0700

    Build out logic for calculator

--- app/js/services.js
+++ app/js/services.js
@@ -72,37 +72,46 @@ angular.module('myApp.services', [])
     };
 
   }])
-  .factory('algorithmSvc', [function() {
+  .factory('algorithmSvc', ['$rootScope', function($rootScope) {
     //returns recommendation string and status
     //3 possible statuses: 'bad', 'ok', 'good'
     var patient = {
-      // patient information:
-      // race
-      // age
-      // bp { [systolic, diastolic, date], [systolic, diastolic, date]}
-      // diabetes (bool)
-      // CKD (bool)
-      // onMedication (bool)
-      // medications = {
-      //  medName : {
-      //    dose: num,
-      //    maxDose: bool,
-      //    unit: 'str' (ml, mg, etc)
-      //   }
-      // }
-      // targetBP: [systolic, diastolic]
-      // isAtBPGoal: bool
+      race: $rootScope.patientData.demographics.Race.Text,
+      age: parseInt($rootScope.patientData.demographics.Age.substring(0,$rootScope.patientData.demographics.Age.length-1)),
+      bp: ,
+      hasDiabetes: (bool),
+      hasCKD (bool),
+      onMedication (bool),
+      medications = {
+       medName : {
+         dose: num,
+         maxDose: bool,
+         unit: 'str' (ml, mg, etc)
+        }
+      }
+      targetBP: [systolic, diastolic]
+      isAtBPGoal: bool
     };
 
     var recMessages = {
       continue: "Continue current treatment and monitoring.",
-      initialVisit: {
-        nonBlackNoCKD: "",
-        blackNoCKD: "",
-        CKD: ""
+      firstVisit: {
+        nonBlackNoCKD: "Initiate thiazide-type diuretic or ACEI or ARB or CCB, alone or in combination. ACEIs and ARBs should not be used in combination.",
+        blackNoCKD: "Initiate thiazide-type diuretic or CCB, alone or in combination.",
+        CKD: "Initiate ACEI or ARB, alone or in combination with other drug class. ACEIs and ARBs should not be used in combination."
       },
-
-
+      titrationStrategies: {
+        a: "Maximize first medication before adding second.",
+        b: "Add second medication before reaching maximum dose of first medication.",
+        c: "Start with two medication classes, separately or as fixed-dose combination."
+      },
+      allFollowUpVisits: "Reinforce medication and lifestyle adherence.",
+      secondVisit: {
+        ab: "Add and titrate thiazide-type diuretic or ACEI or ARB or CCB (use medication class not previously selected and avoid combined use of ACEI and ARB).",
+        c: "Titrate doses of initial medication to maximum."
+      },
+      thirdVisit: "Add and titrate thiazide-type diuretic or ACEI or ARB or CCB (use medication class not previously selected and avoid combined use of ACEI and ARB).",
+      fourthVisit: "Add additional medication class(eg, &#914;-blocker, aldosterone antagonist, or others) and/or refer to physician with expertise in hypertension management."
     };
 
     var hasBPGoal = function(patient){
@@ -128,16 +137,39 @@ angular.module('myApp.services', [])
     if(hasBPGoal(patient)){
       if(isAtBPGoal(patient.currentBP, patient.targetBP)){
         return {
+          // meeting goal -- move to data viz to reinforce success and show BP graphs
+        }
+      } else { // not at BP goals
 
+      }
+    } else {
+      if(patient.age >= 18) {
+        // set targetBP by age and diabetes/CKD logic
+        if(!patient.hasDiabetes && !patient.hasCKD) {
+          if(patient.age >= 60) {
+            patient.targetBP = [150, 90];
+            patient.has
+          } else if (patient.age < 60) {
+            patient.targetBP = [140, 90];
+          }
+        } else if(patient.hasDiabetes || patient.hasCKD) {
+          if(patient.hasDiabetes && !patient.hasCKD) {
+            patient.targetBP = [140, 90];
+          } else if (patient.hasCKD) {
+            patient.targetBP = [140, 90];
+          }
         }
       }
+
+        if(!patient.hasCKD) {
+          
+        }
     }
     //
     // if patient is 18 or over
     //   if no diabetes or CKD
     //     if patient is 60 or older
     //       patient.targetBP = [150, 90]
-    //       if patient.race is not black
     //     else if under 60
     //       patient.targetBP = [140,90]
     //   else if diabetes or CKD
@@ -148,7 +180,7 @@ angular.module('myApp.services', [])
     //
     //   if !CKD
     //     if nonblack
-    //
+    //      
     //     else if black
     //
     //   else if CKD
