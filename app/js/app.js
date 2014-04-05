0bbb1e9ca5671bb373a3074162274532cbe347a0
Hack Reactor Students <github@hackreactor.com>
2014-04-05 11:48:24 -0700

    This refactors patientData onto substrate and fixes #13

--- app/js/app.js
+++ app/js/app.js
@@ -14,14 +14,14 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]).
-run(['$rootScope','algorithmSvc','substrate', function($rootScope, algorithmSvc, substrate) {
+run(['$rootScope','substrate', function($rootScope, substrate) {
   $rootScope.patientId = 3230000;
   $rootScope.calculator = 'JNC8';
 
   substrate.getPatientData($rootScope.patientId, function(patientData) {
     console.log('Into getPD callback...');
-    $rootScope.patientData = patientData;
-    console.log($rootScope.patientData);
+    substrate.patientData = patientData;
+    console.log(substrate.patientData);
   });
 
   // algorithmSvc($rootScope.patientData, 'JNC8');
