e69a331093883cab45c0316acaeee20bab457368
ianlyons <ilyons@gmail.com>
2014-04-04 20:33:40 -0700

    Work on calculator logic

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,16 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('dataEntryCtrl', ['$rootScope', 'substrate',  function($rootScope, substrate) {
+  .controller('dataEntryCtrl', ['$rootScope', function($rootScope) {
     console.log('Controller loaded.');
-    substrate.getPatientData($rootScope.patientId, function(patientData) {
-      console.log('Into getPD callback...')
-      console.log(patientData);
-    });
-    // $rootScope.patientData = substrate.getPatientData($rootScope.patientId);
-    // console.log($rootScope.patientData);
-    // substrate.getLabs($rootScope.patientId);
-    // substrate.getVitals($rootScope.patientId);
   }])
 
   .controller('MyCtrl2', [function() {
