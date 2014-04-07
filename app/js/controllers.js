91cf904fe9f9042b734434e5079dd4e284c02a65
Hack Reactor Students <accounts@hackreactor.com>
2014-04-07 16:50:39 -0700

    Streamline controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,10 @@
 angular.module('myApp.controllers', [
 ])
 .controller('MainController', ['$scope', 'initializedData', function($scope, initializedData) {
+  // console.log($scope.initializedData);
+}])
+.controller('dataEntryCtrl', ['$scope', '$q', 'initializedData',
+function($scope, $q, initializedData) {
   $scope.initializedData = initializedData;
   console.log($scope.initializedData);
-}])
-.controller('dataEntryCtrl', ['$rootScope', '$q',
-function($rootScope, $q) {
-}])
+}]);
