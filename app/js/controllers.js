a362f5526aa3257bc2fa44cfe19765c91bb2ee44
skeller88 <skeller88@gmail.com>
2014-04-07 15:53:01 -0700

    Splash functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,19 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('dataEntryCtrl', ['$rootScope', '$q', 'substrateData', 'dbData',
-  function($rootScope, $q, substrateData, dbData) {
-    console.log('sdata: ', substrateData);
-    console.log('dbdata: ', dbData);
-    console.log('Controller loaded.');
-  }])
+angular.module('myApp.controllers', [
+  ])
 
-  .controller('MyCtrl2', [function() {
-  }]);
+.controller('MainController', function($scope, initializer) {
+
+  // console.log('substrate', substrateData);
+})
+.controller('dataEntryCtrl', ['$rootScope', '$q',
+function($rootScope, $q) {
+  console.log('dbdata: ');
+  console.log('Controller loaded.');
+}])
+
+.controller('MyCtrl2', [function() {
+
+}]);
