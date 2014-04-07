bdc0ecdc6de485e2b88ecce3281a08677fae4315
skeller88 <skeller88@gmail.com>
2014-04-07 16:26:45 -0700

    Clean up service code, refactor routes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,20 +3,11 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [
-  ])
-
-.controller('MainController', function($scope, initializedData) {
-
+])
+.controller('MainController', ['$scope', 'initializedData', function($scope, initializedData) {
   $scope.initializedData = initializedData;
   console.log($scope.initializedData);
-
-})
+}])
 .controller('dataEntryCtrl', ['$rootScope', '$q',
 function($rootScope, $q) {
-  console.log('dbdata: ');
-  console.log('Controller loaded.');
 }])
-
-.controller('MyCtrl2', [function() {
-
-}]);
