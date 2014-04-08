3d51137709e07faa7ec3cc0474e8353b623f6fda
skeller88 <skeller88@gmail.com>
2014-04-07 17:53:52 -0700

    Link database and API requests to views

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,10 +4,16 @@
 
 angular.module('myApp.controllers', [
 ])
-.controller('dataEntryCtrl', ['$scope', '$q', 'startup',
-function($scope, $q, startup) {
-  console.log("ptData:");
-  console.log(startup.ptData);
+.controller('dataEntryCtrl', ['$scope', '$q', 'pt',
+function($scope, $q, pt) {
+  console.log('ptData:');
+  $scope.pt = pt;
+  // $scope.$watch(pt, function(newPt, oldPt) {
+  //   console.log('newPt', newPt);
+  //   console.log('oldPt', oldPt);
+  //   $scope.pt = newPt;
+  // });
+
 }])
 .controller('dataVizCtrl', ['$scope', function($scope) {
 
