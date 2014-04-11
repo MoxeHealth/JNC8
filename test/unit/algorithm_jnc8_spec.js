53d42763336b0aad1bba11995f031f214c3a3831
skeller88 <skeller88@gmail.com>
2014-04-11 12:57:51 -0700

    Algorithm logic in separate .js file. Create medication data structure. Add files to SpecRunner.

--- test/unit/algorithm_jnc8_spec.js
+++ test/unit/algorithm_jnc8_spec.js
@@ -1,4 +1,10 @@
-describe('user', function(){
+describe('algorithm', function(){
+  beforeEach(
+  describe('meds', function(){
+    it('should be an array of objects', function(){
+
+    });
+  });
   it('should work', function(){
     expect(2).toEqual(2);
   });
