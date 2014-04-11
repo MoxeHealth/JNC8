e8576ed21e64fc63d2320a1f204c2ec9dd896632
skeller88 <skeller88@gmail.com>
2014-04-10 20:41:12 -0700

    Change orientation of blood pressure table. Add click dropdown menu to each medication listed.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,6 +10,14 @@ function($scope, $q, $location, pt) {
   };
 
   $scope.pt = pt;
+
+  $scope.liClick = function(){
+    console.log('li click');
+  };
+
+  $scope.spanClick = function(){
+    console.log('span click');
+  };
   
   $scope.buttonsSelected = function() {
     if($scope.pt.hasCKD && $scope.pt.isOnMedication && $scope.pt.hasDiabetes){
@@ -21,12 +29,16 @@ function($scope, $q, $location, pt) {
 
 .controller('dataVizCtrl', ['$scope', 'pt', 'startup', 'algorithm', function($scope, pt, startup, algorithm) {
 
+  //placeholder for now, in the future will be received from algorithm:
+  //$scope.medRecs = algorithm.medRecs;
+  $scope.medRecs = [{'medClass': 'ACE', 'meds': ['benazeprirl', 'captopril']}, {'medClass': 'ARB', 'meds': ['xanax', 'prozac']}];
+
   $scope.recommendationMsg = algorithm.recommendation;
+
   $scope.dbData = startup; // refactor to only expose db data and not substrate data
+  console.log(startup);
   $scope.targetDias = algorithm.targetBP.diastolic;
   $scope.targetSys = algorithm.targetBP.systolic;
   $scope.pt = pt;
-  console.log($scope.pt);
-
 }]);
 
