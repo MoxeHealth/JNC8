03251ca57914f0956208d7be4e88e5c6a743f2a8
Ian <ilyons@gmail.com>
2014-04-11 17:12:39 -0700

    Add directive for GoodRx functionality

--- app/js/directives.js
+++ app/js/directives.js
@@ -10,17 +10,76 @@ angular.module('myApp.directives', []).
     };
   }])
 
+  .directive('drugPricing', ['goodRx', 'pt', function(goodRx, pt) {
+
+    var generateEmailLink = function(email, drugObj){
+      var mailtoString = 'mailto:';
+
+      // append the patient's email
+      if(email.length) {
+        mailtoString += email[0].EmailAddress + '?';
+      } else {
+        mailtoString += '?';
+      }
+
+      if(email.length > 1) {
+        mailtoString += 'cc=' + email[1].EmailAddress + '&';
+      }
+
+      mailtoString += 'subject=Pricing information for ' + drugObj.display + '&';
+
+      // set the email body
+      mailtoString += 'body=Please find the pricing details for ' + drugObj.display + ' at this site: ' + drugObj.url;
+
+
+      return mailtoString;
+    };
+
+
+    return {
+      template: '<div>'+
+      '<div class="med-name" ng-init="showDetails=false" ng-click="showDetails = !showDetails">{{medName}}' + 
+            '<div class="inline-info price right">{{ price | currency }}</div>' + 
+          '</div>' + 
+          '<div class="med-details cf" ng-show="showDetails">' +
+              '<div class="inline-info left"><span class="med-detail">' +
+                '<span class="label">Dosage:</span> {{ dosage }}' +
+              '</span>' +
+              '<span class="med-detail"><span class="label">Units:</span> {{ units }}</span></div>' +
+              '<div class="inline-info right"><span class="med-detail"><a href="{{ drugInfo.url }}" target="_blank">More pricing information</a></span>' +
+              '<span class="med-detail"><a href="{{emailLink}}" title="Email pricing information for {{medName}}" target="_blank">Email pricing details</a></span></div>' +
+          '</div></div>',
+      replace: true,
+      restrict: 'EA',
+      scope: {
+        medName: '=med',
+      },
+      link: function(scope, element, attrs) {
+        console.log(pt);
+        // get the pricing for this drug
+        goodRx.getPricing(scope.medName, function(res) {
+          console.log(res);
+          scope.price = res.data.price[0];
+          scope.drugInfo = res.data;
+          scope.dosage = res.data.dosage;
+          scope.units = res.data.quantity;
+          scope.emailLink = generateEmailLink(pt.email, scope.drugInfo);
+          console.log(scope.emailLink);
+        });
+      }
+    }
+
+  }])
 
   .directive('bpGraph', ['graphHelpers', function(graphHelpers) {
       
       var renderGraph = function(scope) {
 
-      	// set the width/height of the graph based on the size of the containing element
-      	var elWidth = document.getElementsByTagName('bp-graph')[0].clientWidth;
-      	console.log("elWidth: " + elWidth);
+        // set the width/height of the graph based on the size of the containing element
+        var elWidth = document.getElementsByTagName('bp-graph')[0].clientWidth;
 
         //handle case when patient has no entries in database 
-        console.log(scope.data.ptData.db);
+        // console.log(scope.data.ptData.db);
        if(!scope.data.ptData.db.length){ return; }
 
         var data = graphHelpers.parseArray(scope.data.ptData.db);
@@ -96,18 +155,18 @@ angular.module('myApp.directives', []).
         graph.append('svg:path').attr('d', sysLine(data)).attr('class', 'plotline sysLine').attr('transform','translate(40,0)');
         
         graph.append('line')
-        	.attr('x1', 0)
-        	.attr('y1', function() { return y(scope.targetDias); })
-        	.attr('x2', width)
-        	.attr('y2', function() { return y(scope.targetDias); })
-        	.attr('class', 'plotline diasLine targetLine');
+          .attr('x1', 0)
+          .attr('y1', function() { return y(scope.targetDias); })
+          .attr('x2', width)
+          .attr('y2', function() { return y(scope.targetDias); })
+          .attr('class', 'plotline diasLine targetLine');
 
         graph.append('line')
-        	.attr('x1', 0)
-        	.attr('y1', function() { return y(scope.targetSys); })
-        	.attr('x2', width)
-        	.attr('y2', function() { return y(scope.targetSys); })
-        	.attr('class', 'plotline sysLine targetLine');
+          .attr('x1', 0)
+          .attr('y1', function() { return y(scope.targetSys); })
+          .attr('x2', width)
+          .attr('y2', function() { return y(scope.targetSys); })
+          .attr('class', 'plotline sysLine targetLine');
 
     };
 
@@ -129,13 +188,13 @@ angular.module('myApp.directives', []).
       },
       link: function (scope, element, attrs) {
         scope.$watch('targetSys', function(newVal, oldVal) {
-          console.log('targetSys changed.');
+          // console.log('targetSys changed.');
           renderGraph(scope);
           removeFirstGraphChild();
         });
 
         scope.$watch('targetDias', function(newVal, oldVal) {
-          console.log('targetDias changed.');
+          // console.log('targetDias changed.');
           renderGraph(scope);
           removeFirstGraphChild();
         });
