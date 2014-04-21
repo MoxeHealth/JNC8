7b6ad5c6d5376af936e8554044e1bc5ca482ff03
Ian <ilyons@gmail.com>
2014-04-21 13:50:41 -0700

    GoodRx pricing fix

--- encrypt.js
+++ encrypt.js
@@ -1,8 +1,31 @@
 var crypto = require('crypto');
-var server = require('./server')
+var route = require('./route-handler');
+
+api = {
+  moxe: {
+    baseUrl: 'http://substratestaging.moxehealth.com/api',
+    year2013: '/2013-1/get',
+    year2014: '/2014-1/get',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': 'Basic SFJKU0M4VGVzdEFwcDowN2FhODJiZTI4ODY=',
+      'VendorID': 1, //Sandbox ID
+      'OrganizationID': 3, //testing ID
+      'EHRUserId': 'terry', //testing user
+      'ApplicationKey': 'b6956ad2-ca01-45e6-ab57-4b51b99e70df'
+    }
+  },
+  goodRx: {
+    url: 'https://api.goodrx.com/low-price',
+    key: 'ef08ec276e',
+    secret: 'bYiIVq2mv+GsSqtYrjjNqQ=='
+  }
+};
+
 
 exports.signUrl = function(queryString, secret) {
-  var hmac = crypto.createHmac('sha256', server.api.goodRx.secret);
+	console.log(route);
+  var hmac = crypto.createHmac('sha256', api.goodRx.secret);
   hmac.update(queryString);
   var encodedString = makeUrlSafe(hmac.digest('base64'));
   return encodedString;
